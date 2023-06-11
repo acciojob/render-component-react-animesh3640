89be2ac9a8eedf569230c741d0ffcab03fef3ff3
animesh3640 <karne.ani@gmail.com>
2023-06-11 11:06:36 +0530

    first

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React, {Component, useState} from "react";
 import '../styles/App.css';
-
+import NewComponent from "./components/NewComponent";
 const App = () => {
   return (
-    <div id="main"></div>
+    <div id="main">
+      <NewComponent></NewComponent>
+    </div>
   )
 }
 
---
+++ src/components/NewComponent.js
@@ -0,0 +1,10 @@
+
+const NewComponent = ()=>{
+    return(
+        <p>
+            I am learning React. My life is getting better.
+        </p>
+    )
+}
+
+export default NewComponent
