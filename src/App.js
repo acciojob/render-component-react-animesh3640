fcb8905eba3bcd667d3542bbd06b891e3836a8d0
animesh3640 <karne.ani@gmail.com>
2023-06-11 11:12:06 +0530

    second

--- src/App.js
+++ src/App.js
@@ -3,9 +3,7 @@ import '../styles/App.css';
 import NewComponent from "./components/NewComponent";
 const App = () => {
   return (
-    <div id="main">
-      <NewComponent></NewComponent>
-    </div>
+    <NewComponent></NewComponent>
   )
 }
 
